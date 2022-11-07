8093cd68d47c6c478fe9b6a1cfdd36168b2e1740
tadams95 <tyrelleadams28@gmail.com>
2022-11-07 14:10:39 -0800

    Fix useContext, Making Context Dynamic

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,9 +3,9 @@ import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
 
-  const [ctx] = useContext(AuthContext);
+  const ctx = useContext(AuthContext);
 
   return (
     <nav className={classes.nav}>
@@ -22,7 +22,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
