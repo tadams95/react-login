5500acb2ec56f26e05d79f28fbc106d24320a33e
tadams95 <tyrelleadams28@gmail.com>
2022-11-07 13:48:43 -0800

    Using the React Context API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-
     const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
 
     if (storedUserLoggedInInfo === "1") {
@@ -31,13 +31,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
